539b54b280cbb5ea1ba43c845b8c929dfeb68a56
Parry-Zeng <897907871@qq.com>
2021-05-09 23:00:17 +0800

    读取文件

--- test10.js
+++ test10.js
@@ -1,9 +1,21 @@
 // 读取文件
 const fs = require('fs');
-fs.readFile('./index.html', 'utf8', (err, data) => {
+// fs.readFile('./index.html', 'utf8', (err, data) => {
+//     if(err) {
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// })
+
+fs.readFile(__dirname +'/index.html', 'utf8', (err, data) => {
     if(err) {
         console.log(err);
     }else{
         console.log(data);
     }
-})
+});
+// 当前文件的路径
+console.log(__dirname);
+//当前文件的完整路径
+console.log(__filename);
