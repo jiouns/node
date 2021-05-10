0ece2a97c6c8b7dd39de626151e55dabc83e8ba5
Parry-Zeng <897907871@qq.com>
2021-05-10 23:52:22 +0800

    读取文件 + 不同系统下路径兼容性问题

--- test10.js
+++ test10.js
@@ -8,14 +8,25 @@ const fs = require('fs');
 //     }
 // })
 
-fs.readFile(__dirname +'/index.html', 'utf8', (err, data) => {
+// fs.readFile(__dirname +'/index.html', 'utf8', (err, data) => {
+//     if(err) {
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// });
+// // 当前文件的路径
+// console.log(__dirname);
+// //当前文件的完整路径
+// console.log(__filename);
+
+// 解决不同操作系统的兼容性问题，在 macos中路径斜杠为/； windows为\\
+// 加载 path 模块
+const path = require('path');
+fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
     if(err) {
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-});
-// 当前文件的路径
-console.log(__dirname);
-//当前文件的完整路径
-console.log(__filename);
+                console.log(err);
+            }else{
+                console.log(data);
+            }
+})
