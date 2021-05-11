c7fc7780f04d8d78638781693fa9dbe96320e9f2
Parry-Zeng <897907871@qq.com>
2021-05-11 23:19:21 +0800

    http 服务

--- test02.js
+++ test02.js
@@ -4,7 +4,7 @@ const port = 3000; // 端口
 
 const server = http.createServer((req,res) =>{ // 调用 node.js  server 模块
     res.statusCode = 200; //状态码 200 -- 成功
-    res.setHeader('Content-Type','text/plain'); //设置文本类型
+    res.setHeader('Content-Type','text/plain;charset=utf-8'); //设置文本类型,相应报文头
     res.end('hello world!\n'); 
 });
 server.listen(port, hostname, () =>{
