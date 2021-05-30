486f97d15479e93f825d41344db50a354b5ed5a3
Parry-Zeng <897907871@qq.com>
2021-05-30 20:49:32 +0800

    writeHead用法

--- test12.js
+++ test12.js
@@ -12,3 +12,24 @@ http.createServer((req, res) =>{
 }).listen(port,host,() =>{
     console.log(`server runing at http://${host}:${port}`);
 });
+
+// response 常用对象
+
+http.createServer((req, res) =>{
+    // res.setHeader('Content-Type', 'application/json')，设置报文头
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+    //  设置 http 响应状态码
+    res.statusCode = 404;
+    // 响应状态码信息
+    res.statusMessage = 'No found';
+
+    // 直接向服务器写入报文头
+
+    res.writeHead(404, "not found", { 'Content-Type': 'text/plain'
+});
+
+    res.write("hello world!!! 你好，世界！！！");
+    res.end();
+}).listen(port, host, () =>{
+    console.log(`server runing at http://${host}:${port}` );
+})
